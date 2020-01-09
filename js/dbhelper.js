e974482be87a1ebf6b51f3666325157fabeabc64
saher Noureen <sahernoureen1@gmail.com>
2020-01-09 16:06:33 -0600

    links to image changed

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,8 +9,8 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 5500// Change this to your server port
-   return `http://localhost:${port}/data/restaurants.json`;
-  //  return `https://sahernoureen.github.io/Finalproject/data/restaurants.json`;
+  // return `http://localhost:${port}/data/restaurants.json`;
+   return `https://sahernoureen.github.io/Finalproject/data/restaurants.json`;
   }
 
   /**
@@ -151,8 +151,8 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
-  //  return (`https://raw.githubusercontent.com/sahernoureen/Finalproject/master/img/${restaurant.photograph}`);
+   // return (`/img/${restaurant.photograph}`);
+   return (`https://raw.githubusercontent.com/sahernoureen/Finalproject/master/img/${restaurant.photograph}`);
   }
 
   /**
